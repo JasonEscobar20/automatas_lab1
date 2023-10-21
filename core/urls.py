035b1334c5c1491f09c5085b3ec65ff0c11719f4
Jason Escobar <jasonescobar20@outlook.com>
2023-10-21 17:57:44 -0600

    haciendo conversion afn a afd

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from core.views import index_view, read_txt
+from core.views import index_view, read_txt, result_view
 
 
 app_name = 'core'
 
 urlpatterns = [
     path('', index_view),
-    path('result/',read_txt, name='read_result')
+    path('result/',result_view, name='read_result'),
+    # path('result/afn', read_afn, name='read_afn')
 ]
--- core/views.py
+++ core/views.py
@@ -8,7 +8,6 @@ def index_view(request):
 
 def get_content(line):
     content = line
-    print('entra acsa')
 
     if '{' in line:
         first_square_bracket_index = line.index('{') + 1
@@ -22,45 +21,153 @@ def get_content(line):
 
     return content
 
-def get_transition_list(line, quintupla, alphabet):
+def get_transition_list(transition, quintupla, alphabet):
     transition_list = []
-    test = []
-
-
-    split_transition = line.split('),')
+    split_transition = transition.split('),')
+    result = []
     
     for item in split_transition:
         remove_parenthesis = item.replace('(', '').replace(')', '')
         transition_list.append(remove_parenthesis.split(','))
 
-    pruebas = []
-
     for index, value in enumerate(quintupla):
         state = value
-        pruebas.append([state])
-        test_1 = {}
+        result.append([state])
+        states_by_letter = {}
+        
+        for x in alphabet:
+            verify = states_by_letter.get(x, 0)
+            if verify == 0:
+                states_by_letter[x] = []
 
         for item in transition_list:
             state_to_evaluate = item[0]
             letter = item [1]
             result_state = item[2]
-            verify = test_1.get(letter, 0)
-            if verify == 0:
-                test_1[letter] = []
             
-
-            if state == state_to_evaluate:
-                test_1[letter].append(result_state)
-
-        pruebas[index].append(test_1.items())
-    
-    return pruebas
-
-def read_txt(request):
-    get_data = request.POST
-    get_file = request.FILES['filename']
+            verify = states_by_letter.get(letter, 0)
+            if verify == 0:
+                states_by_letter[letter] = []
         
-    test = [line.rstrip() for line in get_file]
+            if state == state_to_evaluate:
+                states_by_letter[letter].append(result_state)
+        
+        result[index].append(states_by_letter.values())
+    
+    return result
+
+def split_transition_func(data):
+    transition_list = []
+    
+    for item in data:
+        remove_parenthesis = item.replace('(', '').replace(')', '')
+        transition_list.append(remove_parenthesis.split(','))
+
+    return transition_list
+
+def get_epsilon_states(transition):
+    split_transition = transition.split('),')
+    transition_list = split_transition_func(split_transition)
+    epsilon_states = []
+    
+    for item in transition_list:
+        state_to_evaluate = item[0]
+        letter = item [1]
+        result_state = item[2]
+        
+        if letter == 'e':
+            epsilon_states.append(result_state)
+    
+    return epsilon_states
+
+def move_function(item, states_no_checked, compositions, data):
+    alphabet = data['alphabet']
+    alphabet.remove('e')
+    
+    state = item['state']
+    composition = item['composition']
+    print(composition)
+    
+    transition_table = data['transition_line']
+    epsilon_states = get_epsilon_states(transition_table)
+    split_transition = transition_table.split('),')
+    transition_table = split_transition_func(split_transition)
+    
+    pruebitas = []
+    
+    for character in alphabet:
+        for x in composition:
+            for y in transition_table:
+                first_state = y[0]
+                letter = y [1]
+                result_state = y[2]
+                
+                if x == first_state and letter == character:
+                    pruebitas.append(result_state)
+                # print(first_state, letter, result_state)
+            
+            
+            
+            # if state == first_state and letter == character:
+            #     print(result_state)
+    
+    for x in epsilon_states:
+        if x not in pruebitas:
+            pruebitas.append(x)
+            
+    pruebitas.sort()
+    
+    if pruebitas not in compositions:
+        i = ord(state[0])
+        i+=1
+        print(chr(i), 'letra siguiente')
+        
+    states_no_checked.remove(item)
+        
+
+def transform_afn_to_afd(data):
+    transition_list = data['transition_line']
+    initial_state = data['initial_state']
+    epsilon_states = get_epsilon_states(transition_list)
+    first_state = 'a'
+    first_composition = [initial_state, *epsilon_states]
+    compositions = [first_composition]
+    states_no_checked = []
+    
+    first_item = {
+        'state': first_state,
+        'composition': first_composition
+    }
+    
+    states_no_checked.append(first_item)
+    
+    while len(states_no_checked) != 0:
+        for x in states_no_checked:
+            state = x['state']
+            move_function(x, states_no_checked, compositions, data)
+            
+        # states_no_checked.pop()
+        
+    
+    nuevos_estados = []
+    
+    print(epsilon_states)
+
+def result_view(request):
+    get_file = request.FILES['filename']
+    get_data = read_txt(get_file)
+    
+    transform_afn_to_afd(get_data)
+    
+    return render(request, 'core/index.html', {
+        'txt_lines': get_data['txt_lines'],
+        'data': get_data,
+        'transition_array': get_data['transition_array']
+    })
+ 
+
+def read_txt(file):    
+    test = [line.rstrip() for line in file]
     data = {}
     txt_lines = []
     quintupla = []
@@ -88,14 +195,15 @@ def read_txt(request):
             continue
         if 'W=' in decode_line:
             transition = get_content(decode_line)
+            data['transition_line'] = transition
             transition_array = get_transition_list(transition, quintupla, alphabet)
             data['transition_array'] = transition_array
             # print(transition_array)
             continue
-            
+    
+    data['txt_lines'] = txt_lines
 
-
-    return render(request, 'core/index.html', {
-        'txt_lines': txt_lines,
-        'data': data
-    })
+    return data
+    
+    
+    
